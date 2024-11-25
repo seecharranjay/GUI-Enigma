7ad44235b5e2981ebf409b83b09e292e9132d360
Jaya Seecharran <jayaseech@gmail.com>
2024-11-24 19:51:14 -0500

    making the buttons

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -1,6 +1,20 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class EnigmaFrame extends JFrame {
+    private JTextField intPosition;
+    private JTextArea input;
+    private JTextArea output;
+    private JComboBox<String> inner;
+    private JComboBox<String> middle;
+    private JComboBox<String> outer;
 
+    private  JButton bEast = new JButton("EAST");
+
+}
+
+/*
 public static void main(String args[]) {
         JFrame f = new JFrame();
         f.setTitle("GUI Enigma"); // sets title that appears on the top bar
@@ -9,3 +23,4 @@ public static void main(String args[]) {
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // makes it so that closing window exits program
         f.setVisible(true);
 }
+*/
